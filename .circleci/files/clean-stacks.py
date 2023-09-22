7ac2036c6dd1605fbfc7ebfd51eba4263caeae1d
Shiro <thanh20080kg@gmail.com>
2023-09-22 14:20:09 +0700

    init

---
+++ .circleci/files/clean-stacks.py
@@ -0,0 +1,7 @@
+﻿import boto3
+    
+cf_client = boto3.client('cloudformation', region_name="us-east-1")
+    
+for stack in cf_client.describe_stacks()['Stacks']:
+        print("deleting {}".format(stack['StackName']))
+        cf_client.delete_stack(StackName=stack['StackName'])
